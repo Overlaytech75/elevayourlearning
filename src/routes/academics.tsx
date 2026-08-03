98b52825434eb80a845729e6e8a5a2f241d3bc33
Sakif Khandaker <sakifkhandaker05@gmail.com>
2026-08-03 00:27:58 +0000

    Changes

--- src/routes/academics.tsx
+++ src/routes/academics.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Plus, Trash2 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
